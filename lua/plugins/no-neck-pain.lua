a8af6bf1fa6c9c5f294a717d4b6a8c0fa885c745
ymich9963 <gmichael834@gmail.com>
2024-08-29 03:28:58 +0100

    Updated no-neck-pai confign

--- lua/plugins/no-neck-pain.lua
+++ lua/plugins/no-neck-pain.lua
@@ -13,6 +13,23 @@ return {
             minSideBufferWidth = 10,
             -- Creates mappings for you to easily interact with the exposed commands.
             --- @type table
+            autocmds = {
+                -- When `true`, enables the plugin when you start Neovim.
+                -- If the main window is  a side tree (e.g. NvimTree) or a dashboard, the command is delayed until it finds a valid window.
+                -- The command is cleaned once it has successfuly ran once.
+                --- @type boolean
+                enableOnVimEnter = false,
+                -- When `true`, enables the plugin when you enter a new Tab.
+                -- note: it does not trigger if you come back to an existing tab, to prevent unwanted interfer with user's decisions.
+                --- @type boolean
+                enableOnTabEnter = false,
+                -- When `true`, reloads the plugin configuration after a colorscheme change.
+                --- @type boolean
+                reloadOnColorSchemeChange = false,
+                -- When `true`, entering one of no-neck-pain side buffer will automatically skip it and go to the next available buffer.
+                --- @type boolean
+                skipEnteringNoNeckPainBuffer = true,
+            },
             mappings = {
                 -- When `true`, creates all the mappings that are not set to `false`.
                 --- @type boolean
