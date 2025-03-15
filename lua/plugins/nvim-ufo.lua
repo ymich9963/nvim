bb73cbf86920aa0f08868a133b2b2cc5abb085a0
ymich9963 <gmichael834@gmail.com>
2025-03-15 00:49:18 +0000

    Minor modifications to things.

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -1,8 +1,7 @@
 return {
     'kevinhwang91/nvim-ufo',
     dependencies = { 'kevinhwang91/promise-async' },
-    lazy = true,
-    event = 'BufEnter',
+    event = 'BufRead',
     config = function ()
         vim.o.foldenable = true
         vim.o.foldcolumn = '0'
