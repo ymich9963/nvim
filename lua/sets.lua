4b60f6946729c8ad5d0529cd71e215270a253ccb
ymich9963 <gmichael834@gmail.com>
2025-04-13 22:32:06 +0100

    Added augroup for setting wrapping in .md files.

--- lua/sets.lua
+++ lua/sets.lua
@@ -23,17 +23,26 @@ vim.opt.updatetime = 50
 vim.opt.termguicolors = true
 vim.opt.autochdir = true
 vim.opt.sessionoptions="blank,buffers,curdir,help,tabpages,winsize,winpos,terminal,localoptions"
+
+-- Forgot what this does
 -- vim.opt.colorcolumn = "80"
+
+-- Hopefully they fix the path issue with this one day
+-- vim.opt.shell = '"C:\\Program Files\\PowerShell\\7\\pwsh.exe" -NoLogo'
 
 -- Setttings to use Powershell, taken from toggleterm.nvim
 vim.opt.shell = vim.fn.executable('pwsh') == 1 and 'pwsh -NoLogo' or 'powershell -NoLogo'
--- Hopefully they fix the path issue with this one day
--- vim.opt.shell = '"C:\\Program Files\\PowerShell\\7\\pwsh.exe" -NoLogo'
 vim.opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
 vim.opt.shellredir = '-RedirectStandardOutput %s -NoNewWindow -Wait'
 vim.opt.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
 vim.opt.shellxquote = ''
 vim.opt.shellquote = ''
+
+-- Something to do with quickfix menu not working
 -- vim.g.compiler_gcc_ignore_unmatched_lines = true
 
+-- Augroup and Autocmd for (un)setting wrapping in Markdown files
+vim.cmd(" augroup md_augroup | autocmd BufRead *.md set wrap | autocmd BufLeave *.md set nowrap")
+
 vim.cmd('colorscheme nanos')
+
