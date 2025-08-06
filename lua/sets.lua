695d449967a5c21e52023b1bf47ecec0f301dad5
ymich9963 <gmichael834@gmail.com>
2025-08-06 14:34:54 +0100

    Simplified config to the options that I understand and that aren't default...

--- lua/sets.lua
+++ lua/sets.lua
@@ -1,44 +1,29 @@
 -- Leader
 vim.g.mapleader = " "
 
--- Cursor
--- Based on the default and another example in the docs, blinks when input and doesn't when no input
-vim.opt.guicursor = "n-v-c-sm:block-Cursor/lCursor,i-ci-ve:block-blinkon300-blinkoff300-blinkwait10,r-cr-o:hor25,t:block-blinkon500-blinkoff500-TermCursor"
--- vim.opt.guicursor = ""
-
 vim.opt.tabstop = 4 -- Tabs and shift
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
-vim.opt.shiftround = true
 vim.opt.smartindent = true -- Indents
--- vim.opt.autoindent = true -- Copy indent from current line
 vim.opt.wrap = false -- Wrap
+vim.opt.ignorecase = true
 vim.opt.number = true -- Line numbers
 vim.opt.relativenumber = true
-vim.opt.backup = false -- Undos and backups
-vim.opt.writebackup = false
 vim.opt.swapfile = false
 vim.opt.undofile = true
-vim.opt.undodir = vim.fn.expand("~/.vim/undodir")
-vim.opt.autowrite = false -- Auto save, read, change directory
-vim.opt.autoread = true
 vim.opt.autochdir = true
-vim.opt.hlsearch = false -- Searching
-vim.opt.incsearch = true
+vim.opt.hlsearch = false -- Searching highlights
 vim.opt.termguicolors = true -- Nice term colours
-vim.opt.foldenable = false
-vim.opt.foldcolumn = '0'
-vim.opt.foldtext = "i"
-vim.opt.foldmethod = "indent"
-vim.opt.foldlevel = 99
 vim.opt.cursorline = true -- Cursor highlight line
 vim.opt.signcolumn = "yes" -- Sign column next to lines
 vim.opt.scrolloff = 10
-vim.opt.showcmd = true
-vim.opt.errorbells = false -- No error bells
-vim.opt.mouse = "a" -- Enable mouse
-vim.opt.sessionoptions="blank,buffers,curdir,help,tabpages,winsize,winpos,terminal,localoptions"
 vim.opt.winborder= "rounded"
+
+vim.opt.foldenable = false
+vim.opt.foldcolumn = '0'
+vim.opt.foldtext = " "
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevel = 99
 
 -- Setttings to use Powershell, taken from toggleterm.nvim
 vim.opt.shell = vim.fn.executable('pwsh') == 1 and 'pwsh -NoLogo' or 'powershell -NoLogo'
@@ -48,10 +33,9 @@ vim.opt.shellxquote = ''
 vim.opt.shellquote = ''
 -- Fixes ANSI escape codes when using PS and :!, taken from https://github.com/ConnorSweeneyDev/.config/issues/2#issuecomment-2209443983
 vim.opt.shellcmdflag = "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;$PSStyle.Formatting.Error = '';$PSStyle.Formatting.ErrorAccent = '';$PSStyle.Formatting.Warning = '';$PSStyle.OutputRendering = 'PlainText';"
--- Hopefully they fix the path issue with this one day
--- vim.opt.shell = '"C:\\Program Files\\PowerShell\\7\\pwsh.exe" -NoLogo'
+-- Check this issue to see if pwsh can finally be used with :te and no :te pwsh, https://github.com/neovim/neovim/issues/31494
 
--- Adds Prompt markers to pwsh shell, use [[ or ]] to navigate
+-- Adds Prompt markers to shell, use [[ or ]] to navigate
 vim.api.nvim_create_autocmd('TermOpen', {
     command = 'setlocal signcolumn=auto',
 })
