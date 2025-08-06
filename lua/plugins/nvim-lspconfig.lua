6d76f7cceb1eb3f5f3d71e9b6cf890942899d2d8
ymich9963 <gmichael834@gmail.com>
2025-08-06 15:07:18 +0100

    Further simplified config and added markview.

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,11 +1,4 @@
 return {
     "neovim/nvim-lspconfig",
-    event = "VimEnter",
-    config = function ()
-    end,
-
-    keys = {
-        {"<leader>fd", function() vim.diagnostic.open_float(nil, {focus=false}) end, desc = "Open diagnostics in float window"},
-    }
 }
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,23 +1,11 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    event = 'VimEnter',
     config = function()
         require'nvim-treesitter.configs'.setup {
-            -- A list of parser names, or "all" (the five listed parsers should always be installed)
-            ensure_installed = {"c", "cpp", "python", "json", "markdown", "markdown_inline", "javascript", "lua", "vim", "vimdoc", "doxygen", "html"},
-            -- 'latex' requires the tree-sitter CLI to be installed
-
-            -- Install parsers synchronously (only applied to `ensure_installed`)
+            ensure_installed = {"c", "cpp", "python", "json", "markdown", "markdown_inline", "javascript", "lua", "vim", "vimdoc", "doxygen", "html"}, -- 'latex' requires the tree-sitter CLI to be installed
             sync_install = false,
-
             auto_install = true,
-
-            highlight = {
-                enable = true,
-                -- additional_vim_regex_highlighting = { 'markdown', "markdown_inline" },
-           },
+            highlight = { enable = true, },
         }
-        -- vim.cmd([[:TSUpdate]])
     end,
-    -- lazy = true,
 }
