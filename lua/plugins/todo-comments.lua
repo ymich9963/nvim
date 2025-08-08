c4aa99e5469bf6a70ffea9ad37af90dc045d383e
ymich9963 <gmichael834@gmail.com>
2025-08-08 10:29:07 +0100

    TODO comments wouldn't load. Fixed the lazy loading.

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -2,9 +2,6 @@ return {
     "folke/todo-comments.nvim",
     opts = {
         signs = false, -- show icons in the signs column
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
     },
-    event = "BufReadPost",
+    event = "BufRead",
 }
