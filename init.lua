e3dc7d0e1b7b01d911b437115ad31618e91ecd3c
ymich9963 <gmichael834@gmail.com>
2025-10-17 15:18:49 +0100

    Removed the auto install because it shows messages every time.

--- init.lua
+++ init.lua
@@ -153,8 +153,6 @@ require("lazy").setup({
 })
 
 vim.cmd('packadd nohlsearch') -- Automatically turn off search highlighting
-
-require("nvim-treesitter").install({ "c", "cpp", "lua", "vim", "vimdoc", "markdown", "markdown-inline", "python", "xml", "powershell" })
 --END-PLUGINS--
 
 --LSP--
