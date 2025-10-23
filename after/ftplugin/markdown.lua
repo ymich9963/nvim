10d9430011159451a2652c17ce936bbaf8bcc84a
ymich9963 <gmichael834@gmail.com>
2025-10-23 16:19:17 +0100

    Added delete/insert hyperlink functions and keymaps.

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -3,7 +3,7 @@ vim.diagnostic.enable(false, { bufnr = 0 }) -- Disable diagnostics for current .
 
 -- Simulate the map gf :e <cfile>.md<CR> so that it works with spaces
 -- Must also remove [[ ]] to make Wikilinks work on Windows
-vim.keymap.set('n', 'gf', function()
+local open_md_file_wikilink = function ()
     local cursor_pos = 0
     local line = vim.api.nvim_get_current_line()
     local current_col = vim.fn.col('.')
@@ -19,25 +19,25 @@ vim.keymap.set('n', 'gf', function()
     if file then
         vim.cmd('edit ' .. file .. '.md')
     end
-end,
-{
-    desc = "Open markdown file from Wikilink",
-    buffer = true,
-})
-
-local selected_text = function()
-    local mode = vim.api.nvim_get_mode().mode
-    -- \22 is an escaped version of <c-v>
-    if mode == "v" or mode == "V" or mode == "\22" then
-        local text = vim.fn.getregion(vim.fn.getpos "v", vim.fn.getpos ".")
-        vim.print(text)
-    end
 end
 
-vim.keymap.set('v', '<C-K>', function()
-    selected_text()
-end,
-{
-    desc = "Select text under cursor",
-    buffer = true,
-})
+local insert_hyperlink = function ()
+    vim.api.nvim_input('s[](<ESC>"+pa)<ESC>F[p')
+end
+
+local delete_hyperlink = function ()
+    vim.api.nvim_input('di[F[vf)p')
+end
+
+vim.keymap.set('n', 'gf', function()
+    open_md_file_wikilink()
+end, { desc = "Open markdown file from Wikilink", buffer = true, })
+
+vim.keymap.set("v", '<C-K>', function()
+    insert_hyperlink()
+end, { desc = "Insert hyperlink", buffer = true, })
+
+vim.keymap.set("n", '<C-K>', function()
+    delete_hyperlink
+end, { desc = "Delete hyperlink", buffer = true, })
+
