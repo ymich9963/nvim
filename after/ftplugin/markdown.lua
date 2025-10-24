82bd18839a98e80c0b740441344573c1c02c4aab
ymich9963 <gmichael834@gmail.com>
2025-10-24 12:37:40 +0100

    Improved MD linking with glorious Lua.

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,42 +2,63 @@ vim.wo.wrap = true -- Enable wrap for current .md window
 vim.diagnostic.enable(false, { bufnr = 0 }) -- Disable diagnostics for current .md buffer
 
 -- Simulate the map gf :e <cfile>.md<CR> so that it works with spaces
--- Must also remove [[ ]] to make Wikilinks work on Windows
-local open_md_file_wikilink = function ()
-    local cursor_pos = 0
+local open_md_file_wikilink = function()
     local line = vim.api.nvim_get_current_line()
     local current_col = vim.fn.col('.')
-    -- Get the current cursor position that is closest to the [[link]]
-    -- This is to prevent issues with multiple links in one line
-    for i = 0, #line do
-        local cc = line:sub(i, i + 1)
-        if cc == "[[" and i < current_col then
-            cursor_pos = i - 1
+
+    for start_pos, file ,end_pos in line:gmatch("()(%[%[(.-)%]%])()") do
+        if start_pos < current_col and end_pos > current_col then
+            vim.cmd('edit ' .. file .. '.md')
         end
-    end
-    local file = string.match(line, "%[%[(.-)%]%]", cursor_pos)
-    if file then
-        vim.cmd('edit ' .. file .. '.md')
     end
 end
 
-local insert_hyperlink = function ()
+local check_md_link = function()
+    local line = vim.api.nvim_get_current_line()
+    local current_col = vim.fn.col('.')
+
+    for start_pos, links, end_pos in line:gmatch("()(%[[^%]]+%]%([^%)]+%)())") do
+        if start_pos < current_col and end_pos > current_col then
+            return true
+        end
+    end
+
+    return false
+end
+
+local insert_hyperlink = function()
+    local reg = vim.fn.getreg('+')
+
+    -- Set if empty
+    if reg == '' then
+        vim.fn.setreg('+','"+ register empty')
+    end
+
+    -- Sanitize text to prevent chaos
+    vim.fn.setreg('+', reg:gsub("[%c]", ""))
+
+    -- TODO: Make this better
     vim.api.nvim_input('s[](<ESC>"+pa)<ESC>F[p')
 end
 
-local delete_hyperlink = function ()
+local delete_hyperlink = function()
+    -- TODO: Make this better
     vim.api.nvim_input('di[F[vf)p')
+end
+
+local toggle_hyperlink = function()
+    if check_md_link() then
+        delete_hyperlink()
+    else
+        insert_hyperlink()
+    end
 end
 
 vim.keymap.set('n', 'gf', function()
     open_md_file_wikilink()
 end, { desc = "Open markdown file from Wikilink", buffer = true, })
 
-vim.keymap.set("v", '<C-K>', function()
-    insert_hyperlink()
-end, { desc = "Insert hyperlink", buffer = true, })
-
-vim.keymap.set("n", '<C-K>', function()
-    delete_hyperlink
-end, { desc = "Delete hyperlink", buffer = true, })
+vim.keymap.set({"v", "n"}, '<C-K>', function()
+    toggle_hyperlink()
+end, { desc = "Toggle hyperlink", buffer = true, })
 
