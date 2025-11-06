52ad7e9009e3e602be217ce22b97aa80c19bb40a
ymich9963 <gmichael834@gmail.com>
2025-11-06 14:32:42 +0000

    Removed InsertDate command. Fixed <C-K> remap.

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,7 +2,7 @@ vim.wo.wrap = true -- Enable wrap for current .md window
 vim.diagnostic.enable(false, { bufnr = 0 }) -- Disable diagnostics for current .md buffer
 
 vim.keymap.set('n', 'gf', ':Mdn open_wikilink<CR>', { desc = "Open markdown file from Wikilink" })
-vim.keymap.set({"v", "n"}, '<C-K>', ':Mdn toggle_hyperlink<CR>', { desc = "Toggle hyperlink" })
+vim.keymap.set({"v", "n"}, "<C-K>", ":Mdn hyperlink_toggle<CR>", { desc = "Toggle hyperlink" })
 vim.keymap.set("n", "<Left>", ":Mdn go_back<CR>", { desc = "Go to back to previously visited Markdown buffer" })
 vim.keymap.set("n", "<Right>", ":Mdn go_forward<CR>", { desc = "Go to next visited Markdown buffer" })
 vim.keymap.set({"v", "n"}, "<C-B>", ":Mdn bold_toggle<CR>", { desc = "Toggle bold formatting" })
