f13b0b99df6b82a33fcc373e8f58d08b15649d3d
ymich9963 <gmichael834@gmail.com>
2025-11-13 14:56:33 +0000

    Figured out how to have wrapping per buffer filetype

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,7 +1,7 @@
-vim.wo.wrap = true -- Enable wrap for current .md window
+vim.wo[vim.api.nvim_get_current_win()][0].wrap = true -- Enable wrap for current .md buffer
 vim.diagnostic.enable(false, { bufnr = 0 }) -- Disable diagnostics for current .md buffer
 
-vim.opt.isfname:remove('[') -- To enable path completion on Windows :h i_CTRL-X_CTRL-F
+vim.opt.isfname:remove('[') -- To allow path completion on Windows :h i_CTRL-X_CTRL-F
 vim.opt.isfname:remove(']')
 
 vim.keymap.set('n', 'gf', ':Mdn open_wikilink<CR>', { buffer = true, desc = "Open markdown file from Wikilink" })
