0e94ffcc842cf28fc4e70bee168533216c11f05f
ymich9963 <gmichael834@gmail.com>
2025-12-10 09:54:45 +0000

    Update shell options based on my PR

--- init.lua
+++ init.lua
@@ -33,14 +33,6 @@ vim.opt.foldmethod = "indent"
 vim.opt.foldlevel = 99
 
 -- Check this issue to see if pwsh can finally be used with :te and no :te pwsh, https://github.com/neovim/neovim/issues/31494
--- important to test :make and :grep when setting these options, try maybe use :set makeprg=echo\ test for testing
--- vim.opt.shell = 'powershell'
--- vim.opt.shellcmdflag = "-NoLogo -NoProfile -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;$PSDefaultParameterValues['Out-File:Encoding']='utf8';"
--- vim.opt.shellpipe  = '> %s 2>&1'
--- vim.opt.shellxquote = ''
--- vim.opt.shellquote = ''
--- vim.opt.shelltemp = false
-
 vim.opt.shell = 'pwsh'
 vim.opt.shellcmdflag = "-NoLogo -NoProfile -NonInteractive -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;$PSDefaultParameterValues['Out-File:Encoding']='utf8';$PSStyle.OutputRendering = 'PlainText';";
 vim.opt.shellpipe  = '> %s 2>&1'
